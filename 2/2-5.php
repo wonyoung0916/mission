591a9547656467d6f23c1f2377f58f7b6532879d
조원영 <wycho@idevel.kr>
2023-12-24 22:26:40 +0900

    .

---
+++ 2/2-5.php
@@ -0,0 +1,24 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>출력결과</title>
+</head>
+<body>
+<script>
+    function solution(arr){
+        let n=arr.length;
+        let answer=Array.from({length:n}, ()=>1);
+        let max =
+
+        for (let i = 0; i < n; i++){
+
+        }
+
+        return answer;
+    }
+
+    let arr=[87, 89, 92, 100, 76];
+    console.log(solution(arr));
+</script>
+</body>
+</html>
